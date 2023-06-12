32446af89be4f62bb76c74825e2e3e07701d60e4
Filippo Maria Bertossi <64846189+pippoberto@users.noreply.github.com>
2023-06-12 19:11:44 +0200

    Update randomNumber.dart

--- randomNumber.dart
+++ randomNumber.dart
@@ -2,11 +2,10 @@ import 'dart:convert';
 import 'dart:math' as math;
 
 int randomNumber() {
-  /// MODIFY CODE ONLY BELOW THIS LINE
 
+  // returns a random number between 1 and 2147483647
   var rng = math.Random();
   int randomNumber = rng.nextInt(2147483647) + 1;
   return randomNumber;
 
-  /// MODIFY CODE ONLY ABOVE THIS LINE
 }
